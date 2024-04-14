35bd451dbbac7ce9ef95aa80e876857145131009
Ahmad Maher <ahmed.maher@178@gmail.com>
2024-04-14 21:19:57 +0200

    added test script for getting most polluted timestamp

--- tests/app.test.js
+++ tests/app.test.js
@@ -1,84 +1,95 @@
 const axios = require('axios');
-const { getAirQuality, getMostPolluted } = require('../controllers/airQuality.controller'); 
-const AirQuality = require('../models/AirQuality'); 
+const {
+	getAirQuality,
+	getMostPolluted,
+} = require('../controllers/airQuality.controller');
+const AirQuality = require('../models/AirQuality');
 
 jest.mock('axios');
 jest.mock('../models/AirQuality');
 
 describe('getAirQuality', () => {
-  it('should return air quality data', async () => {
-    const mockResponse = {
-      data: {
-        data: {
-          current: {
-            pollution: {
-              ts: '2024-04-14T12:00:00.000Z',
-              aqius: 41,
-              mainus: 'p2',
-              aqicn: 14,
-              maincn: 'p2',
-            },
-          },
-        },
-      },
-    };
+	it('should return air quality data', async () => {
+		const mockResponse = {
+			data: {
+				data: {
+					current: {
+						pollution: {
+							ts: '2024-04-14T12:00:00.000Z',
+							aqius: 41,
+							mainus: 'p2',
+							aqicn: 14,
+							maincn: 'p2',
+						},
+					},
+				},
+			},
+		};
 
-    axios.get.mockResolvedValue(mockResponse);
+		axios.get.mockResolvedValue(mockResponse);
 
-    const req = { params: { lat: 'latitude', lng: 'longitude' } };
-    const res = { json: jest.fn() };
+		const req = { params: { lat: 'latitude', lng: 'longitude' } };
+		const res = { json: jest.fn() };
 
-    await getAirQuality(req, res);
+		await getAirQuality(req, res);
 
-    expect(res.json).toHaveBeenCalledWith({
-      result: {
-        pollution: {
-          timestamp: '2024-04-14T12:00:00.000Z',
-          aqius: 41,
-          mainus: 'p2',
-          aqicn: 14,
-          maincn: 'p2',
-        },
-      },
-    });
-  });
+		expect(res.json).toHaveBeenCalledWith({
+			result: {
+				pollution: {
+					timestamp: '2024-04-14T12:00:00.000Z',
+					aqius: 41,
+					mainus: 'p2',
+					aqicn: 14,
+					maincn: 'p2',
+				},
+			},
+		});
+	});
 
-  it('should handle errors', async () => {
-    axios.get.mockRejectedValue(new Error('API error'));
+	it('should handle errors', async () => {
+		axios.get.mockRejectedValue(new Error('API error'));
 
-    const req = { params: { lat: 'latitude', lng: 'longitude' } };
-    const res = { status: jest.fn().mockReturnThis(), json: jest.fn() };
+		const req = { params: { lat: 'latitude', lng: 'longitude' } };
+		const res = { status: jest.fn().mockReturnThis(), json: jest.fn() };
 
-    await getAirQuality(req, res);
+		await getAirQuality(req, res);
 
-    expect(res.status).toHaveBeenCalledWith(500);
-    expect(res.json).toHaveBeenCalledWith({ error: 'Failed to fetch air quality data' });
-  });
+		expect(res.status).toHaveBeenCalledWith(500);
+		expect(res.json).toHaveBeenCalledWith({
+			error: 'Failed to fetch air quality data',
+		});
+	});
 });
 
 describe('getMostPolluted', () => {
-  it('should return the most polluted record', async () => {
-    AirQuality.findOne.mockResolvedValue({ city: 'Paris', timestamp: '2024-04-14T12:00:00.000Z' });
-  
-    const req = { body: { city: 'Paris' } };
-    const res = { json: jest.fn(), status: jest.fn().mockReturnThis() }; // Mock the status function
-  
-    await getMostPolluted(req, res);
-  
-    expect(res.status).toHaveBeenCalledWith(500); 
-    expect(res.json).toHaveBeenCalledWith({ error: 'Failed to fetch air quality data for Paris' });
-  });
-  
+	it('should return the most polluted record', async () => {
+		AirQuality.findOne.mockResolvedValue({
+			city: 'Paris',
+			timestamp: '2024-04-14T12:00:00.000Z',
+		});
 
-  it('should handle no record found', async () => {
-    AirQuality.findOne.mockResolvedValue(null);
+		const req = { body: { city: 'Paris' } };
+		const res = { json: jest.fn(), status: jest.fn().mockReturnThis() }; // Mock the status function
 
-    const req = { body: { city: 'NonexistentCity' } };
-    const res = { status: jest.fn().mockReturnThis(), json: jest.fn() };
+		await getMostPolluted(req, res);
 
-    await getMostPolluted(req, res);
+		expect(res.status).toHaveBeenCalledWith(500);
+		expect(res.json).toHaveBeenCalledWith({
+			error: 'Failed to fetch air quality data for Paris',
+		});
+	});
 
-    expect(res.status).toHaveBeenCalledWith(500);
-    expect(res.json).toHaveBeenCalledWith({ error: 'Failed to fetch air quality data for Paris' });
-  });
+	it('should handle no record found', async () => {
+		AirQuality.findOne.mockResolvedValue(null);
+
+		const req = { body: { city: 'NonexistentCity' } };
+		const res = { status: jest.fn().mockReturnThis(), json: jest.fn() };
+
+		await getMostPolluted(req, res);
+
+		expect(res.status).toHaveBeenCalledWith(500);
+		expect(res.json).toHaveBeenCalledWith({
+			error: 'Failed to fetch air quality data for Paris',
+		});
+	});
 });
