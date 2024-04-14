7675a3c149a798599d8d1fd78519f6ff711cc1c9
Ahmad Maher <ahmed.maher@178@gmail.com>
2024-04-14 20:36:36 +0200

    configured server and database

---
+++ index.js
@@ -0,0 +1,36 @@
+require('dotenv').config();
+
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+
+const routes = require('./routes');
+
+const mongoose = require('mongoose');
+
+// Parse JSON bodies
+app.use(bodyParser.json());
+
+// Parse URL-encoded bodies
+app.use(bodyParser.urlencoded({ extended: true }));
+
+const { databaseConfig, serverConfig } = require('./config');
+
+const { checkAirQualityParis } = require('./cron/checkAirQualityParis');
+
+app.listen(serverConfig.port, function(){
+    console.log('listening on port ' + serverConfig.port);
+})
+
+app.use("/api", routes);
+
+checkAirQualityParis();
+
+mongoose.connect(databaseConfig.uri);
+
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', () => {
+  console.log('Connected to MongoDB');
+});
+
