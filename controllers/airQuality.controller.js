95a4983119f9692aaa09221a35057ee38a61cdb9
Ahmad Maher <ahmed.maher@178@gmail.com>
2024-04-14 21:18:24 +0200

    addded error handling

--- controllers/airQuality.controller.js
+++ controllers/airQuality.controller.js
@@ -2,56 +2,54 @@ const axios = require('axios');
 const { serverConfig } = require('../config');
 const AirQuality = require('../models/AirQuality');
 
-
 const getAirQuality = async (req, res) => {
-    try {
-        const { lat, lng } = req.params;
-        const apiUrl = `http://api.airvisual.com/v2/nearest_city?lat=${lat}&lon=${lng}&key=${serverConfig.iqairApiKey}`;
+	try {
+		const { lat, lng } = req.params;
+		const apiUrl = `http://api.airvisual.com/v2/nearest_city?lat=${lat}&lon=${lng}&key=${serverConfig.iqairApiKey}`;
 
-        const response = await axios.get(apiUrl);
-        const airQualityData = response.data.data;
-    
-        res.json({
-            "result": {
-              "pollution": {
-              "timestamp": airQualityData.current.pollution.ts,
-              "aqius": airQualityData.current.pollution.aqius,
-              "mainus": airQualityData.current.pollution.mainus,
-              "aqicn": airQualityData.current.pollution.aqicn,
-              "maincn": airQualityData.current.pollution.maincn
-              }
-            }
-        });
-      } catch (error) {
-        console.log('Error fetching air quality data:', error);
-        res.status(500).json({ 
-          error: 'Failed to fetch air quality data' 
-        });
-      }
+		const response = await axios.get(apiUrl);
+		const airQualityData = response.data.data;
+
+		res.json({
+			result: {
+				pollution: {
+					timestamp: airQualityData.current.pollution.ts,
+					aqius: airQualityData.current.pollution.aqius,
+					mainus: airQualityData.current.pollution.mainus,
+					aqicn: airQualityData.current.pollution.aqicn,
+					maincn: airQualityData.current.pollution.maincn,
+				},
+			},
+		});
+	} catch (error) {
+		console.log('Error fetching air quality data:', error);
+		res.status(500).json({
+			error: 'Failed to fetch air quality data',
+		});
+	}
 };
 
 const getMostPolluted = async (req, res) => {
-  try {
-
-    AirQuality.findOne({ city: req.body.city })
-    .sort({ aqius: -1 })
-    .exec()
-    .then((highestPollutionRecord) => {
-      if (highestPollutionRecord) {
-        res.json( {
-          "result": highestPollutionRecord.timestamp
-        });     
-      } else {
-        res.status(500).json({ 
-          error: 'No existing record yet, try again in one minute' 
-        });
-      }
-    })
-  } catch (error) {
-    res.status(500).json({ 
-      error: 'Failed to fetch air quality data for Paris' 
-    });
-  }
+	try {
+		AirQuality.findOne({ city: req.body.city })
+			.sort({ aqius: -1 })
+			.exec()
+			.then((highestPollutionRecord) => {
+				if (highestPollutionRecord) {
+					res.json({
+						result: highestPollutionRecord.timestamp,
+					});
+				} else {
+					res.status(500).json({
+						error: 'No existing record yet, try again in one minute',
+					});
+				}
+			});
+	} catch (error) {
+		res.status(500).json({
+			error: 'Failed to fetch air quality data for Paris',
+		});
+	}
 };
 
 module.exports = { getAirQuality, getMostPolluted };
